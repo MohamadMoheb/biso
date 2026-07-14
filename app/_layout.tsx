543e9218da8efbe8f3fb330bb0bb065ea35d8c73
Mohamad Moheb <mohamadxmoheb@gmail.com>
2026-07-14 12:16:44 +0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,21 @@
-﻿import 'react-native-gesture-handler';
+import 'react-native-gesture-handler';
 
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
+import { SettingsProvider } from '../src/settings/SettingsContext';
+
 export default function RootLayout() {
   return (
-    <>
+    <SettingsProvider>
       <StatusBar hidden />
-      <Stack screenOptions={{ headerShown: false, animation: 'fade' }} />
-    </>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          animation: 'fade',
+          orientation: 'portrait',
+        }}
+      />
+    </SettingsProvider>
   );
 }
