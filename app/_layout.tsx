883043f5ee9a24e7ccc215881142dea2922bc65d
Mohamad Moheb <mohamadxmoheb@gmail.com>
2026-07-14 23:01:08 +0300

    Update app/_layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,24 @@
 import 'react-native-gesture-handler';
 
+import { loadAsync } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 
-import '../src/audio/preloadSounds';
+import { preloadSounds } from '../src/audio/preloadSounds';
+import { prefetchBackgroundTiles } from '../src/components/ThemeBackground';
 import { SettingsProvider } from '../src/settings/SettingsContext';
 
 export default function RootLayout() {
+  useEffect(() => {
+    void loadAsync({
+      Ionicons: require('@react-native-vector-icons/ionicons/fonts/Ionicons.ttf'),
+    }).catch(() => undefined);
+
+    preloadSounds();
+    prefetchBackgroundTiles();
+  }, []);
+
   return (
     <SettingsProvider>
       <StatusBar hidden />
